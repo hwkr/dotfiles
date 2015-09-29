05a3aeb5c3b8064f3355b17e12f9f53b758371d3
Ben Hawker <ben@hawker.cc>
2015-09-29 15:09:57 -0700

    Tweak git prompt script colors

--- scripts/utilities/gitprompt.sh
+++ scripts/utilities/gitprompt.sh
@@ -8,42 +8,40 @@ function get_git_prompt() {
 
         # Parse the Status
         status=`git status 2>&1 | tee`
-        dirty=`echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?"`
-        untracked=`echo -n "${status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?"`
+        
         ahead=`echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?"`
         behind=`echo -n "${status}" 2> /dev/null | grep "Your branch is behind" &> /dev/null; echo "$?"`
-        newfile=`echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?"`
-        renamed=`echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?"`
-        deleted=`echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?"`
+        diverged=`echo -n "${status}" 2> /dev/null | grep "have diverged" &> /dev/null; echo "$?"`
+        
+        untracked=`echo -n "${status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?"`
+        unstaged=`echo -n "${status}" 2> /dev/null | grep "Changes not staged for commit" &> /dev/null; echo "$?"`
+        tocommit=`echo -n "${status}" 2> /dev/null | grep "Changes to be committed" &> /dev/null; echo "$?"`
+        
+        # dirty=`echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?"`
+        # newfile=`echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?"`
+        # renamed=`echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?"`
+        # deleted=`echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?"`
 
         # Set the Prompt
         
         # Check branch status
         if [ "${ahead}" == "0" ]; then
             ICON="\xE2\x86\x91"
-        fi
-        if [ "${behind}" == "0" ]; then
+        elif [ "${behind}" == "0" ]; then
             ICON="\xE2\x86\x93"
-        fi
-        if [ "${ahead}" == "0" ] && [ "${behind}" == "0" ]; then
-            ICON="\xE2\x86\x95"
+        elif [ "${diverged}" == "0" ]; then
+            ICON="\xE2\x86\x97"
         fi
         
         # Check file status
         if [ "${untracked}" == "0" ]; then
             COLOR="${RED}"
         fi
-        if [ "${renamed}" == "0" ]; then
+        if [ "${unstaged}" == "0" ]; then
             COLOR="${YEL}"
         fi
-        if [ "${newfile}" == "0" ]; then
-            COLOR="${YEL}"
-        fi
-        if [ "${deleted}" == "0" ]; then
-            COLOR="${YEL}"
-        fi
-        if [ "${dirty}" == "0" ]; then
-            COLOR="${YEL}"
+        if [ "${tocommit}" == "0" ]; then
+            COLOR="${BGRN}"
         fi
         # if [ "${ICON}" != "" ]; then
             # ICON="$ICON"
