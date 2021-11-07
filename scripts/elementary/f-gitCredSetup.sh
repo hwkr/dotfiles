8c363160ea86c0400bbd99f0c90b25c6099d6e8c
Brynn Hawker <brynn@hawker.me>
2021-11-06 21:11:44 -0700

    Delete old files

--- scripts/elementary/f-gitCredSetup.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get install libgnome-keyring-dev
-cd /usr/share/doc/git/contrib/credential/gnome-keyring
-sudo make
-git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
